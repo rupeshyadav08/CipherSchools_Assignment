ece45bbb7d22f50eebcb3e89dd40a12df03a8a27
Yadav Rupesh Mohanlal <yadavrupeshmohanlal@Yadavs-MacBook-Air.local>
2021-02-17 01:57:25 +0530

    tenth commit

--- assignment2/decoding.cpp
+++ assignment2/decoding.cpp
@@ -8,33 +8,38 @@ int decode(string data,int k,int hash []){
     {
         return 1;
     }
+
     string s1=data.substr(0,1);
-    if(s1 == "0"){
+
+    if(s1 == "0")
+    {
        return 0;
     }
-    
+
+    if (hash[k]!=0){
+        return hash[k];
+    }
   
     int result = decode(data,k-1,hash);
     string s2=data.substr(0,2);
 
     int n=stoi(s2);
-    if (k>=2 && n<=26){
+
+    if (k>=2 && n<=26)
+    {
         result=result+decode(data,k-2,hash);
     }
+    hash[k]=result;
     return result;
     
 
 }
 int main_decoder(string data,int n){
-   int hash[n+1];
-
-   for(int i=0;i<n;i++){
-       hash[i]=0;
-   }
+   int hash[n+1]={0};
     return decode(data,n,hash);
 }
 int main(){
-string s= "12";
+string s= "1217";
     int n = s.length();
     cout << "Count is " << main_decoder(s, n);
     return 0;
